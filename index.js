26d472929b95333dfa083b2425058e4907853ef1
dashcaj <158211825+dashcaj@users.noreply.github.com>
2024-10-11 13:57:34 -0400

    ftyjy

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const DOMSelectors = {
-    header: document.querySelector(".title"),
 /*     cardHeader: document.querySelector(".cardheader"), */
+    header: document.querySelector(".title"),
     button: document.querySelector("button"),
     form: document.querySelector(".form"),
-    container: document.querySelector(".container")
+    container: document.querySelector(".container"),
+    input: document.querySelector("#input"),
+    input2: document.querySelector("#input2")
 }
 
+DOMSelectors.container.insertAdjacentHTML("beforeend",
+    `<div class="card">
+        <img class="pictures1" src="https://tinyurl.com/3t8d6k56" alt="Chicken">
+        <div>
+        <h2 class="name">gkj</h2>
+        </div>
+    </div>`
+)
 
 DOMSelectors.button.addEventListener("click", function(event){
     event.preventDefault()
-
+    let input = DOMSelectors.input.value;
+    let input2 = DOMSelectors.input2.value;
+    console.log(input);
+    console.log(input2);
 })
+
+
 
 
 
