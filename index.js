4889e56cbfcd63160662f593a12bf84c1d7110a5
dashcaj <dustinc7@nycstudents.net>
2024-10-14 23:35:18 -0400

    uhihuo

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
 const DOMSelectors = {
-/*     cardHeader: document.querySelector(".cardheader"), */
     header: document.querySelector(".title"),
     button: document.querySelector("button"),
-    form: document.querySelector(".form"),
     container: document.querySelector(".container"),
-    input: document.querySelector("#input"),
-    input2: document.querySelector("#input2")
+    inputname: document.getElementById("inputname"),
+    inputpos: document.getElementById("inputpos"),
+    inputimg: document.getElementById("input-img"),
+    form: document.getElementById('hello'),
+};
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const inputs = {
+        name: DOMSelectors.inputname.value,
+        position: DOMSelectors.inputpos.value,
+        image: DOMSelectors.inputimg.value
+    };
+    console.log(inputs.name, inputs.position, inputs.image);
+    createCard(inputs);
+    clearForm();
 }
 
-DOMSelectors.container.insertAdjacentHTML("beforeend",
-    `<div class="card">
-        <img class="pictures1" src="https://tinyurl.com/3t8d6k56" alt="Chicken">
+function createCard({ name, position, image }) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+<div class="box">
+        <h2>${name}</h2>
+        <p>${position}</p>
+        <img class="pictures1" src="${image}" alt="${name}">
         <div>
-        <h2 class="name">gkj</h2>
+        <button class="deletebtn">Delete</button>
         </div>
-    </div>`
-)
+</div>
+    `;
+    DOMSelectors.container.appendChild(card);
 
-DOMSelectors.button.addEventListener("click", function(event){
-    event.preventDefault()
-    let input = DOMSelectors.input.value;
-    let input2 = DOMSelectors.input2.value;
-    console.log(input);
-    console.log(input2);
-})
+    const deleteButton = card.querySelector('.deletebtn');
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    });
+}
+
+function clearForm() {
+    DOMSelectors.inputname.value = '';
+    DOMSelectors.inputpos.value = '';
+    DOMSelectors.inputimg.value = '';
+}
+
+DOMSelectors.form.addEventListener('submit', handleFormSubmit);
+
+
+/* let image = DOMSelectors.inputimg
+
+DOMSelectors.container.insertAdjacentHTML("beforeend",`
+    <div class="card">
+        <h2> ${} </h2>
+        <img src="${}" alt="Image" class ="image">
+    
+    </div>;
+`
+)
+ */
+
 
 
 
